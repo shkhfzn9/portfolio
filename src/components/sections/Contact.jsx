7e19086e7fafbd84911df953242ae57077f18b0d
Sheikh Faizan <fzcodx@gmail.com>
2025-08-29 13:55:16 +0530

    done2

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -129,23 +129,23 @@ const Contact = ({ id }) => {
               >
                 {isLoading ? (
                   <>
-                    <svg 
-                      className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" 
-                      xmlns="http://www.w3.org/2000/svg" 
-                      fill="none" 
+                    <svg
+                      className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
                       viewBox="0 0 24 24"
                     >
-                      <circle 
-                        className="opacity-25" 
-                        cx="12" 
-                        cy="12" 
-                        r="10" 
-                        stroke="currentColor" 
+                      <circle
+                        className="opacity-25"
+                        cx="12"
+                        cy="12"
+                        r="10"
+                        stroke="currentColor"
                         strokeWidth="4"
                       ></circle>
-                      <path 
-                        className="opacity-75" 
-                        fill="currentColor" 
+                      <path
+                        className="opacity-75"
+                        fill="currentColor"
                         d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
                       ></path>
                     </svg>
@@ -218,6 +218,15 @@ const Contact = ({ id }) => {
                   className="bg-gray-800 hover:bg-gray-900 text-white w-10 h-10 rounded-full flex items-center justify-center transition duration-300"
                 >
                   <i className="fab fa-x"></i>
+                </a>
+
+                <a
+                  href="https://github.com/shkhfzn9"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="bg-gray-800 hover:bg-gray-900 text-white w-10 h-10 rounded-full flex items-center justify-center transition duration-300"
+                >
+                  <i className="fab fa-github"></i>
                 </a>
               </div>
             </div>
